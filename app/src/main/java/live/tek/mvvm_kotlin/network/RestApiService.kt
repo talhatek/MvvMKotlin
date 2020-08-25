ae21d924c27dac05ad81be5f36213b8c74bcbc40
talhatek <talhatek@gmail.com>
2020-08-25 19:42:13 +0300

    network timeout set from dell

--- app/src/main/java/live/tek/mvvm_kotlin/network/RestApiService.kt
+++ app/src/main/java/live/tek/mvvm_kotlin/network/RestApiService.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 import live.tek.mvvm_kotlin.model.Post
+import live.tek.mvvm_kotlin.utils.Const
 import live.tek.mvvm_kotlin.utils.Resource
 
 class RestApiService {
@@ -12,20 +14,25 @@ class RestApiService {
     fun getAllPosts(onResult: (Resource<ArrayList<Post>>) -> Unit) {
         val retrofit = ServiceBuilder.buildService(IApi::class.java)
         GlobalScope.launch(Dispatchers.IO) {
-            onResult(Resource.loading(data = null))
-            try {
-                val res = retrofit.getPost()
-                if (res.isSuccessful) {
-                    onResult(Resource.success(data = res.body()) as Resource<ArrayList<Post>>)
-                } else {
-                    Log.e("testing", res.code().toString())
-                    onResult(Resource.error(data = null, message = res.code().toString()))
-                }
+            val task = withTimeoutOrNull(Const.TIME_OUT) {
+                onResult(Resource.loading(data = null))
+                try {
+                    val res = retrofit.getPost()
+                    if (res.isSuccessful) {
+                        onResult(Resource.success(data = res.body()) as Resource<ArrayList<Post>>)
+                    } else {
+                        Log.e("testing", res.code().toString())
+                        onResult(Resource.error(data = null, message = res.code().toString()))
+                    }
 
-            } catch (exception: Exception) {
-                Log.e("testing", exception.message!!)
-                onResult(Resource.error(data = null, message = exception.message.toString()))
+                } catch (exception: Exception) {
+                    onResult(Resource.error(data = null, message = exception.message.toString()))
+                }
             }
+            if (task == null) {
+                onResult(Resource.error(data = null, message = "TimeOut Exception"))
+            }
+
         }
 
 
---
+++ app/src/main/java/live/tek/mvvm_kotlin/utils/Const.kt
@@ -0,0 +1,5 @@
+package live.tek.mvvm_kotlin.utils
+
+object Const {
+    const val TIME_OUT = 3000L
+}
